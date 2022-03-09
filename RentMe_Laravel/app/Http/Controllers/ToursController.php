0280bb6748213f4be92264bab92bdfee3d01e498
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-03-10 01:19:15 +0200

    create model + migration files for each table

---
+++ RentMe_Laravel/app/Http/Controllers/ToursController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tours;
+use Illuminate\Http\Request;
+
+class ToursController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\tours  $tours
+     * @return \Illuminate\Http\Response
+     */
+    public function show(tours $tours)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\tours  $tours
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(tours $tours)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\tours  $tours
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, tours $tours)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\tours  $tours
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(tours $tours)
+    {
+        //
+    }
+}
