827cdb51f296d9cec7eeb2634865f05f91231936
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-03-16 23:00:28 +0200

    Create SignIn page  + background

---
+++ rentme_react/src/pages/SignIn/index.js
@@ -0,0 +1,21 @@
+import './SignIn.css';     
+
+function SignIn() {
+  return (
+    <>
+   
+        <div className="SignIn">
+          <h1>Rent Me</h1>
+          <h3>Let Us Guide You Home</h3>
+
+         
+
+        </div>
+         
+    
+    </>
+
+  );
+}
+
+export default SignIn;
