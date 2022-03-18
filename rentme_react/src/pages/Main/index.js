67fa3d09aa3baa53a2e8ed7d2d6e379d4e7666de
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-03-18 18:47:47 +0200

    Adding Icons + Styling Main Page

--- rentme_react/src/pages/Main/index.js
+++ rentme_react/src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import Navbar from '../../components/Navbar'
 import './Main.css'
 import { Grid, TextField, Button, Card, CardContent, Typography } from '@mui/material/';
+import { FaFacebookSquare,FaInstagram,FaTwitterSquare } from "react-icons/fa";
 
 function Main() {
 
@@ -11,7 +12,7 @@ function Main() {
         <Navbar />
 
         <Grid container spacing={2}>
-              <Grid item xs={12} style={{color:"white", marginTop:"10%"}}>
+              <Grid item xs={12} style={{color:"white", marginTop:"8%"}}>
                 <Grid>
                 <p className="title">Rent Me</p>
                 <p className="slogan">Let Us Guide You Home</p>
@@ -25,14 +26,22 @@ function Main() {
                   <TextField style={{backgroundColor:"white"}}  placeholder="Where Do You Want To Live Next? " label="Location" variant="outlined"  fullWidth required />
                 </Grid>
                 <Grid item xs={6}>
-                  <Button className="search-btn" type="submit" variant="contained" color="primary"  fullWidth>Find Now!</Button>
+                  <Button className="search-btn" type="submit" variant="contained" color="primary"  fullWidth>Rooms</Button>
                 </Grid>
                 <Grid item xs={6}>
-                  <Button className="search-btn" type="submit" variant="contained" color="primary"  fullWidth>Find Now!</Button>
+                  <Button className="search-btn" type="submit" variant="contained" color="primary"  fullWidth>Price</Button>
                 </Grid>
                 <Grid item xs={12}>
                   <Button className="search-btn" type="submit" variant="contained" color="primary"  fullWidth>Find Now!</Button>
                 </Grid>
+                
+                <div className='iconsdiv'>
+                    <FaFacebookSquare  size={42}/>
+                    <FaInstagram size={42} />
+                    <FaTwitterSquare size={42}/>
+                </div>
+                
+             
            
         
                
