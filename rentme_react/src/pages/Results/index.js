9dbb25e909673d0754e5b2b3efcd480c808ec208
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-03-19 01:02:16 +0200

    Styling Results Pages

--- rentme_react/src/pages/Results/index.js
+++ rentme_react/src/pages/Results/index.js
@@ -1,4 +1,5 @@
 import Navbar from '../../components/Navbar'
+
 import { Grid, TextField, Button, Card, CardContent, Typography } from '@mui/material/';
 import { FaFacebookSquare,FaInstagram,FaTwitterSquare } from "react-icons/fa";
 
@@ -7,11 +8,45 @@ function Results() {
   return (
 
     <div className='Result-background'>
-
         <Navbar />
 
-    </div>
+        <Grid container spacing={2} style={{padding:"4%"}}>
+            
+              <Grid item xs={12}  style={{display:"block"}} >
+                <form className="search-results">
+                    <Grid item xs={6} style={{paddingRight:"10px"}} >
+                    <TextField style={{backgroundColor:"white"}}  placeholder="Where Do You Want To Live Next? " label="Location" variant="outlined"  fullWidth required />
+                    </Grid>
+                    <Grid item xs={2}>
+                    <Button className="search-btn" type="submit" variant="contained" color="primary"  fullWidth>Rooms</Button>
+                    </Grid>
+                    <Grid item xs={2}>
+                    <Button className="search-btn" type="submit" variant="contained" color="primary"  fullWidth>Price</Button>
+                    </Grid>
+                    <Grid item xs={2}>
+                    <Button className="search-btn" type="submit" variant="contained" color="primary"  fullWidth>Find Now!</Button>
+                    </Grid>
+           
+                </form>
+              </Grid>
 
+            <Grid item xs={12} style={{color:"white",display:"block"}}>
+                <h1 className="result_title">Search Results</h1>
+            </Grid>
+
+            <Grid item xs={4} style={{color:"white",display:"block"}}>
+                <h1 className="result_title">Search Results</h1>
+            </Grid>
+            <Grid item xs={4} style={{color:"white",display:"block"}}>
+                <h1 className="result_title">Search Results</h1>
+            </Grid>
+            <Grid item xs={4} style={{color:"white",display:"block"}}>
+                <h1 className="result_title">Search Results</h1>
+            </Grid>
+        
+        </Grid>
+       
+        </div>
 
   );
 }
