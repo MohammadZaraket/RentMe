c0a93f5aa0b3d785769ec2a4489928b2cdb3187f
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-03-19 03:48:02 +0200

    Styling Apartment Card

--- rentme_react/src/components/ApartmentCard/index.js
+++ rentme_react/src/components/ApartmentCard/index.js
@@ -11,6 +11,7 @@ import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { FaFacebookSquare,FaInstagram,FaTwitterSquare,BsSearch,FaBed,FaBath } from "react-icons/fa";
+import { Grid} from '@mui/material/';
 
 
 
@@ -37,22 +38,45 @@ function ApartmentCard() {
         <CardMedia
         component="img"
         height="194"
-        image="/static/images/cards/paella.jpg"
+        image="../../assets/test.jpg"
         alt="Paella dish"
+        style={{padding:"10px"}}
       />
       <CardHeader
         title="Apartment Title Here"
         subheader="Best Deal Apartment"
+        id="apartment-title"
       />
       
       <CardContent>
-        <Typography variant="body2" color="text.secondary">
-           Number <FaBed size={35}/>
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-            Number <FaBath size={35} />
-        </Typography>
+      <Grid container spacing={1}>
+              <Grid item xs={6}>
+                <Typography variant="body2" color="text.secondary">
+                     4 <FaBed size={30}/>
+                </Typography>
+              </Grid>
+    
+              <Grid item xs={6}>
+                <Typography variant="body2" color="text.secondary">
+                    2 <FaBath size={30} />
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Typography variant="body2" className="apartment-space">
+                   Space Here
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Typography variant="body2"  className="apartment-price">
+                    Price Here
+                </Typography>
+              </Grid>
+
+        </Grid>
       </CardContent>
+
       <CardActions disableSpacing>
         <ExpandMore
           expand={expanded}
@@ -68,27 +92,10 @@ function ApartmentCard() {
           <Typography paragraph>Description:</Typography>
           <Typography paragraph>
             Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
+            aside for 10 minutes.large plate and set aside, leaving chicken and chorizo in the pan. Add
             pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
             stirring often until thickened and fragrant, about 10 minutes. Add
             saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
           </Typography>
         </CardContent>
       </Collapse>
