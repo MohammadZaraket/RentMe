845113d5f78721ce4d9121d33bb36e366426bb2f
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-03-19 21:54:33 +0200

    Styling Details Page

--- rentme_react/src/pages/Details/index.js
+++ rentme_react/src/pages/Details/index.js
@@ -21,23 +21,64 @@ function Details() {
                 <Grid item xs={6}>
                     <Grid container spacing={1}>
                         <Grid item xs={12}>
-                            <Typography variant="body2" color="text.secondary">
-                            <h1>Apartment Title Here</h1>
-                                <hr/>
-                            </Typography>
+                            <h1 className='details-title'>Apartment Title Here</h1>
+                            <hr className='line' />
                         </Grid>
 
-                        <Grid item xs={3}>
+                        <Grid item xs={2}>
                             <Typography variant="body2" color="text.secondary">
                                 4 <FaBed size={30}/>
                             </Typography>
                         </Grid>
     
-                        <Grid item xs={3}>
+                        <Grid item xs={2}>
                             <Typography variant="body2" color="text.secondary">
                                 2 <FaBath size={30} />
                             </Typography>
                         </Grid>
+
+                        <Grid item xs={12}>
+                            <h1 className="details-space">
+                                Space Here sqm
+                            </h1>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <h1 className="details-price">
+                                Price Here$/Month
+                            </h1>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <p className="details-description">
+                            simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                        <Typography variant="body2" className="agent">
+                              <b>Agent</b>
+                            </Typography>
+                            <p className="agent-name">
+                            Mohammad Zaraket
+                            </p>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                        
+                            <Button className="request-btn" type="submit" variant="contained" color="primary"  fullWidth> <b>Request Tour</b> </Button>
+
+                        </Grid>
+
+                        <Grid item xs={12}>
+
+                        <p className="agent-name">
+                            Location
+                        </p>
+                      
+                        </Grid>
+
+
                     </Grid>
                 </Grid>
 
