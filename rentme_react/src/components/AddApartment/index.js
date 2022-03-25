c8f293f6f0e725742943e2bccc8198fe254fce21
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-03-25 18:47:19 +0200

    Before Adding upload images

--- rentme_react/src/components/AddApartment/index.js
+++ rentme_react/src/components/AddApartment/index.js
@@ -1,8 +1,17 @@
-import * as React from 'react';
 import { Grid, TextField, Button, Card, CardContent, Typography, Modal,Box } from '@mui/material/';
 import { GrClose } from "react-icons/gr";
 
+import React, { useState, useEffect} from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Input from '@mui/material/Input';
 
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+
+import { styled } from '@mui/material/styles';
 
 const style = {
   position: 'absolute',
@@ -19,14 +28,31 @@ const style = {
 };
 
 
+
 export default function AddApartment() {
+
+const [selectedImage, setSelectedImage] = React.useState('');
   const [open, setOpen] = React.useState(false);
+  const [image,setImage] = useState("");
   const handleOpen = () => {
     setOpen(true);
   };
   const handleClose = () => {
     setOpen(false);
   };
+
+
+  
+  function onFileChange(e) {
+    let files = e.target.files;
+    let fileReader = new FileReader();
+    fileReader.readAsDataURL(files[0]);
+
+    fileReader.onload = (event) => {
+        setSelectedImage(event.target.result)
+        console.log(selectedImage);
+    }
+}
 
   return (
     <div style={{ display: "flex", flexDirection: "column",justifyContent: "center"}} >
@@ -45,7 +71,7 @@ export default function AddApartment() {
         
         </Grid>
 
-          <Grid container spacing={1} style={{padding:"2% 0%"}}>
+          <Grid container spacing={0} style={{padding:"2% 0%"}}>
 
             <Grid item xs={12} sm={6}>
               <Grid container spacing={1}>
@@ -57,17 +83,41 @@ export default function AddApartment() {
                 </Grid>
 
                 <Grid item xs={6}>
-                  <Typography variant="body2"  component="p" gutterBottom>
-                    <b>BedRooms </b> 
-                  </Typography> 
-                  <TextField className="modal-field" placeholder="BedRooms"  variant="outlined"  fullWidth required />
+                    <Typography variant="body2"  component="p" gutterBottom>
+                        <b>BedRooms</b> 
+                    </Typography> 
+                        <Box fullWidth>
+                            <FormControl fullWidth >
+                                <Select>
+                                <MenuItem value=""><em>None</em></MenuItem>
+                                <MenuItem value={1}>1</MenuItem>
+                                <MenuItem value={2}>2</MenuItem>
+                                <MenuItem value={3}>3</MenuItem>
+                                <MenuItem value={4}>4</MenuItem>
+                                <MenuItem value={5}>5</MenuItem>
+                                <MenuItem value={6}>6</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Box>
                 </Grid>
 
                 <Grid item xs={6}>
                   <Typography variant="body2"  component="p" gutterBottom>
                     <b>BathRooms </b> 
                   </Typography> 
-                  <TextField className="modal-field" placeholder="BathRooms"  variant="outlined"  fullWidth required />
+                  <Box fullWidth>
+                            <FormControl fullWidth >
+                                <Select>
+                                <MenuItem value=""><em>None</em></MenuItem>
+                                <MenuItem value={1}>1</MenuItem>
+                                <MenuItem value={2}>2</MenuItem>
+                                <MenuItem value={3}>3</MenuItem>
+                                <MenuItem value={4}>4</MenuItem>
+                                <MenuItem value={5}>5</MenuItem>
+                                <MenuItem value={6}>6</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Box>
                 </Grid>
 
               </Grid>
@@ -101,7 +151,7 @@ export default function AddApartment() {
               </Grid>
             </Grid>
 
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12} sm={12}>
               <Grid container spacing={1}>
                
               <Grid item xs={12}>
@@ -112,19 +162,34 @@ export default function AddApartment() {
                 </Grid>
               </Grid>
             </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12} sm={12}>
               <Grid container spacing={1}>
                
-              <Grid item xs={12}>
+              <Grid item xs={4}>
                   <Typography variant="body2"  component="p" gutterBottom>
-                    <b>Price </b> 
+                    <b>Image 1 </b> 
                   </Typography> 
-                  <TextField className="modal-field" placeholder="Price"  variant="outlined"  fullWidth required />
+                  <TextField className="modal-field" placeholder="Image 1"  variant="outlined"  fullWidth required />
                 </Grid>
-
+                <Grid item xs={4}>
+                  <Typography variant="body2"  component="p" gutterBottom>
+                    <b>Image 2 </b> 
+                  </Typography> 
+                  
+                    <label htmlFor="icon-button-file">
+                        <input type="file" className="form-control" name="image" onChange={onFileChange} />
+                    </label>
+                    
+                  <TextField className="modal-field" placeholder="Image 2"  variant="outlined"  fullWidth required />
+                </Grid>
+                <Grid item xs={4}>
+                  <Typography variant="body2"  component="p" gutterBottom>
+                    <b>Image 3 </b> 
+                  </Typography> 
+                  <TextField className="modal-field" placeholder="Image 3"  variant="outlined"  fullWidth required />
+                </Grid>
               </Grid>
             </Grid>
-
 
 
             <Grid item xs={12} style={{display:"flex",justifyContent:"center"}}>
